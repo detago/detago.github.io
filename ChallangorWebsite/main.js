4901963cc7babd76d978383f42cdbb51d9b5cfe8
detago <tdsfortrox@gmail.com>
2023-01-10 22:04:55 +0100

    Initial Commit

---
+++ ChallangorWebsite/main.js
@@ -0,0 +1,13 @@
+﻿let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+let challange =
+    [
+        'Challange 1',
+        'Challange 2',
+        'Challange 3',
+    ];
+
+btn.addEventListener('click', function(){
+    var randomChallange = challange[Math.floor(Math.random() * challange.length)]
+    output.innerHTML = randomChallange;
+})
