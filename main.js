eefd1f2ac94b4bc3a703830421ae196d15bda018
detago <72993728+detago@users.noreply.github.com>
2023-02-11 18:36:37 +0100

    Paulinas Bday

--- main.js
+++ main.js
@@ -4,16 +4,58 @@ let rst = document.getElementById('rst');
 
 let highPropChallenges =
     [
+        // Paulinas Bday
+        'Take a photo with someone',
+        'Give someone a compliment',
+        'Make Lars a compliment',        
+        'Play Rock Paper Scissors with someone',
+        'Be silent for 2 min',
+        'Tell someone a joke',
+        'Tell someone a fun fact',
+        'Trade an article of clothing',
+        'Take a shot without using your hands',
+        'Give someone a hug',
+        'Next time someone tries to talk to you bark at them',
+        'Yell out the first thing that comes to your mind',
+        'Put 5 different available liquids into a cup and drink it',
+        'Let someone DM someone else from your account',
+        'Get at least 3 other people to dance with you',
+        'You cannot drink your drink by yourself for 10 minutes, someone else has to feed it to you',
+        'Ask for ice at the bar, put it in your mouth until it melts',
+        'Do 10 push ups',
+        'Wear your shirt backwards/inside out for a few minutes',
+        'Use a pickup line on someone',
+        'Ask someone if you can sit on their lap (and do it if yes)',
+        'Give someone a kiss (can be anywhere, mouth, cheek, forehead, hand, etc.)',
+        'Smack someones ass',
+        'Call a friend and tell them you cannot talk right now',
+        'Play a round of Tischkicker',
+        'Play a round of beer pong/rage cage/flip cup',
+        'Sing along to a song with someone (you can request a song at the bar)',
+        'Ask Paulina for the camera and take some photos ',
+        'Start a conversation with someone you do not know (or know least)',
+        'Say something weird/completely out of context to someone and walk away ',
+        
+        //Lars Bday
+        /*
         'Let someone choose a shot you drink',
         'Let someone mix you a longdrink',
         'Drink a shot with someone you do not know',
         'Play a round of rage cage',
         'Teach someone your best dance move',
         'Take a selfie with someone you do not know',
+        */
     ];
 
 let lowPropChallenges =
     [
+        //Paulinas Bday
+        'Try to sell something to someone or do a trade',
+        'Drink from the funnel',
+        'Down your drink',
+        
+        //Lars Bday
+        /*
         'Mix someone a longdrink',
         'Convince 2 others to use the dancefloor with you until the end of the song',
         'Drink a shot with someone you know',
@@ -29,6 +71,7 @@ let lowPropChallenges =
         'Sing along to a song with someone. (You may request a song at the bar)',
         'Start a conversation with a random fact',
         'Tell a really bad joke',
+        */
     ];
 
 
@@ -37,7 +80,8 @@ document.getElementById("counter").innerHTML = _counter;
 var lastRandomChallenge = "";
 
 btn.addEventListener('click', function () {
-    selectChallengeType = Math.floor(Math.random()*2);
+    //Changed from 2 to 1.01 for Paulinas Bday
+    selectChallengeType = Math.floor(Math.random()*1.01);
 
     if (selectChallengeType == 0) {
         randomChallenge = highPropChallenges[Math.floor(Math.random() * highPropChallenges.length)];
